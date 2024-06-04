9a08c67e992b7565ddfc5b3b131ee701a2843ba6
MyITSoft Company <myitsoft@mail.ru>
2024-06-04 14:05:36 +0400

    init

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import "../Styles/Header.css"
-const Header = (props) => {
+const Header = () => {
 
     const tg = window. Telegram.WebApp;
-    const onClose = () => {
-        tg. close ()
-    }
     return(
         <header className="Header">
-            <button>Close</button>
             <span className="userName">{tg.initDataUnsafe?.user?.username}</span>
         </header>
     )
